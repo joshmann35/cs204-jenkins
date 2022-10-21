6badfa3efb21adcb48d9a7cc50da81f4dcf25d37
Josh Mann <josh.mann35@hotmail.com>
2022-10-21 13:32:27 -0600

    Add main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,28 +1,36 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        switch (args[0]) {
-            case "add":
-                System.out.println(calculator.add(Integer.parseInt(args[1]), Integer.parseInt(args[2])));
-                break;
-            case "subtract":
-                System.out.println(calculator.subtract(Integer.parseInt(args[1]), Integer.parseInt(args[2])));
-                break;
-            case "multiply":
-                System.out.println(calculator.multiply(Integer.parseInt(args[1]), Integer.parseInt(args[2])));
-                break;
-            case "divide":
-                System.out.println(calculator.divide(Integer.parseInt(args[1]), Integer.parseInt(args[2])));
-                break;
-            case "binary":
-                System.out.println(calculator.intToBinaryNumber(Integer.parseInt(args[1])));
-                break;
-            case "fib":
-                System.out.println(calculator.fibonacciNumberFinder(Integer.parseInt(args[1])));
-                break;
-            default:
-                System.out.println("Unknown method");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a command:");
+        while(true) {
+            String in = scanner.nextLine();
+            String[] inargs = in.split("\\s+");
+            switch (inargs[0]) {
+                case "add":
+                    System.out.println(calculator.add(Integer.parseInt(args[1]), Integer.parseInt(args[2])));
+                    break;
+                case "subtract":
+                    System.out.println(calculator.subtract(Integer.parseInt(args[1]), Integer.parseInt(args[2])));
+                    break;
+                case "multiply":
+                    System.out.println(calculator.multiply(Integer.parseInt(args[1]), Integer.parseInt(args[2])));
+                    break;
+                case "divide":
+                    System.out.println(calculator.divide(Integer.parseInt(args[1]), Integer.parseInt(args[2])));
+                    break;
+                case "binary":
+                    System.out.println(calculator.intToBinaryNumber(Integer.parseInt(args[1])));
+                    break;
+                case "fib":
+                    System.out.println(calculator.fibonacciNumberFinder(Integer.parseInt(args[1])));
+                    break;
+                default:
+                    System.out.println("Unknown method");
+            }
         }
     }
 
