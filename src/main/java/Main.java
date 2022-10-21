4fd8dc70fa694ea565b812ab85a0d1a6848cfaa6
Josh Mann <josh.mann35@hotmail.com>
2022-10-21 13:36:42 -0600

    Add main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,24 +9,27 @@ public class Main {
         while(true) {
             String in = scanner.nextLine();
             String[] inargs = in.split("\\s+");
+            for(String a : inargs) {
+                System.out.println(a);
+            }
             switch (inargs[0]) {
                 case "add":
-                    System.out.println(calculator.add(Integer.parseInt(args[1]), Integer.parseInt(args[2])));
+                    System.out.println(calculator.add(Integer.parseInt(inargs[1]), Integer.parseInt(inargs[2])));
                     break;
                 case "subtract":
-                    System.out.println(calculator.subtract(Integer.parseInt(args[1]), Integer.parseInt(args[2])));
+                    System.out.println(calculator.subtract(Integer.parseInt(inargs[1]), Integer.parseInt(inargs[2])));
                     break;
                 case "multiply":
-                    System.out.println(calculator.multiply(Integer.parseInt(args[1]), Integer.parseInt(args[2])));
+                    System.out.println(calculator.multiply(Integer.parseInt(inargs[1]), Integer.parseInt(inargs[2])));
                     break;
                 case "divide":
-                    System.out.println(calculator.divide(Integer.parseInt(args[1]), Integer.parseInt(args[2])));
+                    System.out.println(calculator.divide(Integer.parseInt(inargs[1]), Integer.parseInt(inargs[2])));
                     break;
                 case "binary":
-                    System.out.println(calculator.intToBinaryNumber(Integer.parseInt(args[1])));
+                    System.out.println(calculator.intToBinaryNumber(Integer.parseInt(inargs[1])));
                     break;
                 case "fib":
-                    System.out.println(calculator.fibonacciNumberFinder(Integer.parseInt(args[1])));
+                    System.out.println(calculator.fibonacciNumberFinder(Integer.parseInt(inargs[1])));
                     break;
                 default:
                     System.out.println("Unknown method");
